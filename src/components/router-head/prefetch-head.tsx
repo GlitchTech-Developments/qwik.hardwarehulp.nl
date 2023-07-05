3c26225311b7c10cb4cdd446dad8b5344ebf7ec5
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-07-05 09:16:54 +0200

    fix: improved font load

--- src/components/router-head/prefetch-head.tsx
+++ src/components/router-head/prefetch-head.tsx
@@ -1,12 +1,14 @@
 const PrefetchHead = () => {
 	return (
 		<>
+			<link rel="dns-prefetch" href="https://fonts.gstatic.com" />
 			<link
-				rel="dns-prefetch"
-				href="https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyC0ITw.woff2"
+				rel="preload"
+				href="https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyC0ITw.woff2?display=swap"
+				as="font"
+				type="font/woff2"
 			/>
 		</>
 	);
 };
-
 export default PrefetchHead;
