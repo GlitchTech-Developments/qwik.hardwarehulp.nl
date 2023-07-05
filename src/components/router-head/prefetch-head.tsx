5d199c6bfd75fc4f00033d4136c99d92ef48c4ab
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-07-05 09:47:05 +0200

    cleanup: removed google prefetch call

--- src/components/router-head/prefetch-head.tsx
+++ src/components/router-head/prefetch-head.tsx
@@ -1,14 +1,4 @@
 const PrefetchHead = () => {
-	return (
-		<>
-			<link rel="dns-prefetch" href="https://fonts.gstatic.com" />
-			<link
-				rel="preload"
-				href="https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyC0ITw.woff2?display=swap"
-				as="font"
-				type="font/woff2"
-			/>
-		</>
-	);
+	return <></>;
 };
 export default PrefetchHead;
